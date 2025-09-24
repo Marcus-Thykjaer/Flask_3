b40cc9dd920e6f69408f3127d0c01b0deb9e09c6
Marcus-Thykjaer <marcus@thykjaer.dk>
2025-09-24 11:12:01 +0200

    la til tre til sider og linket alle sammen med href

--- app.py
+++ app.py
@@ -14,5 +14,35 @@ def index():
     </ul>
     """
 
+@app.route('/aboutme')
+def aboutme():
+     return """
+    <h1>dette er min veldig fine nettside</h1>
+    <p>denne nettsiden er om meg</p>
+    <ul>
+        <li><a href="/">Hjem</a></li>
+    </ul>
+    """
+
+@app.route('/Fisker')
+def Fisker():
+     return """
+    <h1>dette er min veldig fine nettside om kule fisker</h1>
+    <p>fisker er velldig kule</p>
+    <ul>
+        <li><a href="/">Hjem</a></li>
+    </ul>
+    """
+
+@app.route('/Dyr')
+def Dyr():
+     return """
+    <h1>dette er min veldig fine nettside om dyr</h1>
+    <p>denne nettsiden er om dyr de er ikke like kule som fisker</p>
+    <ul>
+        <li><a href="/">Hjem</a></li>
+    </ul>
+    """
+
 if __name__ == '__main__':
     app.run(debug=True)
